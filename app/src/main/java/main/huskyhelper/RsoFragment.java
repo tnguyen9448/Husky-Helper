9e8221696bbb307e617f7a7dc466c49c9606ac1d
anha1104 <aha68145@gmail.com>
2024-05-09 16:27:23 -0700

    fix intent filter commit

--- app/src/main/java/main/huskyhelper/RsoFragment.java
+++ app/src/main/java/main/huskyhelper/RsoFragment.java
@@ -1,5 +1,7 @@
 package main.huskyhelper;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -19,7 +21,26 @@ public class RsoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rso, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_rso, container, false);
+
+
+        view.findViewById(R.id.gamedev).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the link here
+                openLink("https://dubnet.tacoma.uw.edu/login_only?redirect=https%3a%2f%2fdubnet.tacoma.uw.edu%2ffeeds%3ftype%3dclub%26type_id%3d35465%26tab%3dhome"); // Replace with your actual link
+            }
+        });
+
+        return view;
+    }
+
+    private void openLink(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 
 }
